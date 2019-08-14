d471a180d78c520fa5a20f2ae1675584700e6181
hendricendrix <hendricentrix@gmail.com>
2019-08-13 21:29:21 -0700

    Message of Love

--- js/app.js
+++ js/app.js
@@ -1,10 +1,46 @@
 'use strict';
 
-var likesGameOfThrones = prompt('Do you like Game Of Thrones?');
-// likesGameOfThrones = likesGameOfThrones.toLowerCase();
+var likesPokemon = prompt('Do you like Pokemon?');
+// likesPokemon = likesPokemon.toUpperCase();
 
-if(likesGameOfThrones.toUpperCase() === 'YES'){
-  alert('I like Game Of Thrones, Too');
+if(likesPokemon.toLowerCase() === 'YES'){
+  alert('I like Pokemon, Too');
+} else {
+  alert('What do you mean?!');
+}
+
+var likesDinosaurs = prompt('Do you like Dinosaurs?');
+// likesDinosaurs = likesDinosaurs.toUpperCase();
+
+if(likesDinosaurs.toLowerCase() === 'YES'){
+  alert('I like spiky dinosaurs, Too');
+} else {
+  alert('What do you mean?!');
+}
+
+var likesRacecars = prompt('Do you like Racecars?');
+// likesRacecars = likesRaceCars.toLowerCase();
+
+if(likesRacecars.toUpperCase() === 'YES'){
+  alert('I like red racecars, Too');
+} else {
+  alert('What do you mean?!');
+}
+
+var likesColdWeather = prompt('Do you like Cold Weather?');
+// likesDinosaurs = likesDinosaurs.toLowerCase();
+
+if(likesColdWeather.toUpperCase() === 'YES'){
+  alert('I like snowstorms');
+} else {
+  alert('What do you mean?!');
+}
+
+var likesJewelry = prompt('Do you like Jewelry?');
+// likesJewelry = likesJewelry.toLowerCase();
+
+if(likesJewelry.toUpperCase() === 'YES'){
+  alert('I like shiny things, Too');
 } else {
   alert('What do you mean?!');
 }
@@ -13,6 +49,3 @@ if(likesGameOfThrones.toUpperCase() === 'YES'){
 //   do something
 // } else if(a differnt condition is met){
 //   do something else
-// } else {
-//   do something else
-// } 
