3f4d59b553c694f36e6c7b6c8ce8912158e9e38f
hendricendrix <hendricentrix@gmail.com>
2019-08-17 13:37:32 -0700

    Lab201 Guessing

--- js/app.js
+++ js/app.js
@@ -1,51 +1,136 @@
 'use strict';
+function helloEveryonw(name) {
+  return 'hello ' + name;
+}
 
-var likesPokemon = prompt('Do you like Pokemon?');
-// likesPokemon = likesPokemon.toUpperCase();
+helloEveryone('Shubham');
 
-if(likesPokemon.toLowerCase() === 'YES'){
-  alert('I like Pokemon, Too');
+
+
+
+
+
+// Yes or No Questions
+var userName = prompt('Can you guess my what local food I like?');
+// userName = UserName.toUpperCase();
+if(userName.toLowerCase() === 'yes'){
+  alert('Okay lets begin with the local favorites');
+} else {
+  alert('Sure you can, give it a try');
+}
+if(userName.toLowerCase() === 'yes'){
+  alert('There are so many local food choices');
+}
+
+var likesBurgers = prompt('Are there delicious local Burgers?');
+// likesBurgers = likesBurgers.toUpperCase();
+if(likesBurgers.toLowerCase() === 'YES'){
+  alert('I like cheese burgers with double meat!');
+} else {
+  alert('Of course there are!');
+}
+
+var likesBurgers = ['yes', 'no'];
+var i = 0; 
+while(i < likesBurgers.length){
+  if(i !== 1){
+  console.log(likesBurgers[i]);
+  }
+  i++;
+}
+
+//var likesBurgers = ['cheese' ,'plain' , 'ham' ];
+// index           0       1           2  
+//var x = 0;
+//while(x < 10){
+  //console.log('x is ' + x);
+  //x++;
+//}
+
+var likesMexfood = prompt('Do you like Mexican food?');
+// likesMexfood = likesMexfood.toLowerCase();
+if(likesMexfood.toUpperCase() === 'YES'){
+  alert('My favorite dish is enchiladas');
 } else {
   alert('What do you mean?!');
 }
 
-var likesDinosaurs = prompt('Do you like Dinosaurs?');
-// likesDinosaurs = likesDinosaurs.toUpperCase();
+var likesMexfood = ['Yes' , 'No'];
+var i = 0; 
+while(i < likesMexfood.length){
+  if(i !== 1){
+  console.log(likesMexfood[i]);
+  }
+  i++;
+}
 
-if(likesDinosaurs.toLowerCase() === 'YES'){
-  alert('I like spiky dinosaurs, Too');
+var likesFriedfood = prompt('Do you like Fried food?');
+// likesFriedfood = likesFriedfood.toLowerCase();
+if(likesFriedfood.toUpperCase() === 'YES'){
+  alert('My favorite fried foods are vegetables!');
 } else {
   alert('What do you mean?!');
 }
 
-var likesRacecars = prompt('Do you like Racecars?');
-// likesRacecars = likesRaceCars.toLowerCase();
-
-if(likesRacecars.toUpperCase() === 'YES'){
-  alert('I like red racecars, Too');
-} else {
-  alert('What do you mean?!');
+var likesFriedfood = ['Yes' , 'No'];
+var i = 0; 
+while(i < likesFriedfood.length){
+  if(i !== 1){
+  console.log(likesFriedfood[i]);
+  }
+  i++;
 }
 
-var likesColdWeather = prompt('Do you like Cold Weather?');
-// likesDinosaurs = likesDinosaurs.toLowerCase();
+=prompt('aaa' + 'kmkmk?'
 
-if(likesColdWeather.toUpperCase() === 'YES'){
-  alert('I like snowstorms');
-} else {
-  alert('What do you mean?!');
+connsole.log('user ss' + sss);
+
+alert('sssl, sss' + 'dmimimi');
+3
+
+// Question Six
+
+var myFavNumber = 223;
+
+for (var i = 0; i < 4; i++) {
+  var userFavNumber = prompt( 'Guess how many steaks have I ever had in my life?');
+
+  userFavNumber = 
+  console.log('uer fav number' +typeof);
+  userFavNumber);
+
+  userFavNumber = Number(userFavNumber);
+
+  console.log('');
+  console.log('the type of the user number after ' + typeof userFavNumber)
+
+  //console.log('the user answer to Fav Number is, userFavNumber);
+
+  if (userFavNumber === Number (userFavNumber);
+
+  if (userFavNumber === Number MyFavNumber) {
+    alert('Yay! You are right!');
+    break;
+  } else if (userFavNumber < myFavNumber) {
+    alert('Too low!');
+  } else if (userFavNumber . myFavNumber) {
+};
+
+// Question 7
+
+var myTravelList = ['ss,','ssss','sssss']
+
+var userScore = 0;
+//var userScore = parseInt(userScore);
+
+for var j = 0; j < 6; j++) {
+  var usertravelGuess = prompt('What ssss');
+
+  // normalizing the data
+  userTravelGuess - userTravelGuess = 
+  if (isCorrect) {
+    alert('You got it!');
+    userScore++;
+    break;
+  }
 }
-
-var likesJewelry = prompt('Do you like Jewelry?');
-// likesJewelry = likesJewelry.toLowerCase();
-
-if(likesJewelry.toUpperCase() === 'YES'){
-  alert('I like shiny things, Too');
-} else {
-  alert('What do you mean?!');
-}
-
-// if(condition is met){
-//   do something
-// } else if(a differnt condition is met){
-//   do something else
